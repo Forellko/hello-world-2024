79c5bf22effaaad91ae5b9424233b061c80164d6
forellko <dragonforce97q@gmail.com>
2024-01-11 18:55:06 +0300

    Create Dog or Cat project

---
+++ projects/dog-or-cat/main.js
@@ -0,0 +1,24 @@
+const randomButton = document.querySelector('#random-button');
+const randomBlock = document.querySelector('#random-block');
+const image = document.querySelector('#image');
+
+const pathArr = [
+  './images/dog0.jpg',
+  './images/dog1.jpg',
+  './images/dog2.jpeg',
+  './images/dog3.jpg',
+  './images/dog4.jpg',
+  './images/dog5.jpg',
+  './images/cat6.jpg',
+  './images/cat7.jpg',
+  './images/cat8.jpg',
+  './images/cat9.jpg',
+  './images/cat10.jpg',
+];
+
+randomButton.onclick = () => {
+  const rand = Math.round(Math.random() * 9);
+  image.src = pathArr[rand];
+  randomBlock.innerHTML = rand;
+};
+;
