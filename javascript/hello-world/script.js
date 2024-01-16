05152c070fe52d5335df55201cbb73edef490978
forellko <dragonforce97q@gmail.com>
2024-01-16 15:07:10 +0300

    Create elements in block

--- javascript/hello-world/script.js
+++ javascript/hello-world/script.js
@@ -1,2 +1,10 @@
 const block = document.querySelector('#block');
 block.innerHTML = 'Hello JavaScript!';
+
+const arr = ['hello', 'world', 'block1', 'block2'];
+
+arr.forEach((element) => {
+  const docElement = document.createElement('div');
+  docElement.innerHTML = element;
+  block.appendChild(docElement);
+});
