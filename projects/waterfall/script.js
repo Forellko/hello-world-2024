c5aa531fa529d18b376895f2b66d467f45ce3ac3
forellko <dragonforce97q@gmail.com>
2024-01-18 00:29:50 +0300

    Create waterfall project. Add block move down.

---
+++ projects/waterfall/script.js
@@ -0,0 +1,5 @@
+const block = document.createElement('div');
+const main = document.querySelector('#main');
+block.className = 'block';
+block.style.animationDuration = '10s';
+main.appendChild(block);
