5dd5f919dab8503e0cc281cc5c1caaf1447a3294
forellko <dragonforce97q@gmail.com>
2024-01-18 01:01:05 +0300

    Create falls color blocks on all sides

--- projects/waterfall/script.js
+++ projects/waterfall/script.js
@@ -1,5 +1,43 @@
-const block = document.createElement('div');
 const main = document.querySelector('#main');
-block.className = 'block';
-block.style.animationDuration = '10s';
-main.appendChild(block);
+const colors = [
+  'red',
+  'green',
+  'blue',
+  'yellow',
+  'pink',
+  '#FF1493',
+  '#2F4F4F',
+  '#4B0082',
+  '#F08080',
+  '#7B68EE',
+  '#FF4500',
+  '#98FB98',
+];
+const direction = ['falldown', 'fallright', 'fallup', 'fallleft'];
+setInterval(() => {
+  main.innerHTML = '';
+  for (let i = 0; i < 100; i++) {
+    const block = document.createElement('div');
+    block.className = 'block';
+    block.style.backgroundColor =
+      colors[Math.round(Math.random() * (colors.length - 1))];
+    block.style.animationName = direction[Math.round(Math.random() * 3)];
+    block.style.animationDuration = Math.round(Math.random() * 10) + 's';
+    if (block.style.animationName === 'falldown') {
+      block.style.left = Math.round(Math.random() * 100) + 'vw';
+    }
+    if (block.style.animationName === 'fallup') {
+      console.log('fallup');
+      block.style.bottom = 0;
+      block.style.left = Math.round(Math.random() * 100) + 'vw';
+    }
+    if (
+      block.style.animationName === 'fallright' ||
+      block.style.animationName === 'fallleft'
+    ) {
+      block.style.top = Math.round(Math.random() * 100) + 'vh';
+    }
+
+    main.appendChild(block);
+  }
+}, 5000);
