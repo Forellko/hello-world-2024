86a619d5fe3c9bb8678b8f7af25bc322795b0c6e
forellko <dragonforce97q@gmail.com>
2024-01-19 02:25:31 +0300

    Array findLast

--- javascript/array-search/script.js
+++ javascript/array-search/script.js
@@ -1,9 +1,6 @@
 const arr = [1, 2, 3, 4, 5];
 
 console.log(arr.includes(3));
-console.log(
-  arr.find((element) => {
-    return element === 2;
-  })
-);
+console.log(arr.find((element) => element === 2));
 console.log(arr.findIndex((element) => element === 4));
+console.log(arr.findLast((element) => element === 4));
