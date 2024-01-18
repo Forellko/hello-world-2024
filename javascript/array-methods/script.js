461a2e69ea9cd52234bd623df00995c99e4c1461
forellko <dragonforce97q@gmail.com>
2024-01-19 00:43:35 +0300

    Add toSpliced

--- javascript/array-methods/script.js
+++ javascript/array-methods/script.js
@@ -2,3 +2,7 @@ const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
 // method can be used to add new items to an array:
 // its mutation array
 fruits.splice(1, 1, 'Carrot');
+
+//ES2023 added the Array toSpliced() method as a safe way to splice an array without altering the original array.
+//remove first element
+const result = fruits.toSpliced(0, 1);
