3d7d5bec7e8706f24e06394f9a6a98f6d25fba37
forellko <dragonforce97q@gmail.com>
2024-01-18 17:57:30 +0300

    Get random color

--- projects/waterfall/script.js
+++ projects/waterfall/script.js
@@ -13,16 +13,23 @@ const colors = [
   '#FF4500',
   '#98FB98',
 ];
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 const direction = ['falldown', 'fallright', 'fallup', 'fallleft'];
 setInterval(() => {
   main.innerHTML = '';
   for (let i = 0; i < 100; i++) {
     const block = document.createElement('div');
     block.className = 'block';
-    block.style.backgroundColor =
-      colors[Math.round(Math.random() * (colors.length - 1))];
+    block.style.backgroundColor = getRandomColor();
     block.style.animationName = direction[Math.round(Math.random() * 3)];
-    block.style.animationDuration = Math.round(Math.random() * 10) + 's';
+    block.style.animationDuration = Math.random() * 5 + 1 + 's';
     if (block.style.animationName === 'falldown') {
       block.style.left = Math.round(Math.random() * 100) + 'vw';
     }
