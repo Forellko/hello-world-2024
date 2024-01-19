930b0811f5938731c0b81cf53dd0b8dbbc158e71
forellko <dragonforce97q@gmail.com>
2024-01-19 23:04:26 +0300

    Create array iteration. Add every and some

---
+++ javascript/array-iteration/script.js
@@ -0,0 +1,8 @@
+// every
+const array = [1, 2, 3, 4, 5];
+
+// True if all elements is < 5
+const isEvery = array.every((el) => el < 5);
+
+// True if one element is = 3
+const isSome = array.some((el) => el === 3);
