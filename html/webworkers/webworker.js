aae2e29bec4c0d5bf352df9a95376b59de717ebf
forellko <dragonforce97q@gmail.com>
2024-01-23 23:28:11 +0300

    Web workers do not have access to DOM

--- html/webworkers/webworker.js
+++ html/webworkers/webworker.js
@@ -1,3 +1,12 @@
+/* 
+Since web workers are in external files, they do not have access to the following JavaScript objects:
+
+The window object
+The document object
+The parent object
+
+*/
+
 var i = 0;
 
 function timedCount() {
